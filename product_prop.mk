07aa6f7cf1a9382a3b52bb87023763da8d14d0f4
Kishore Srivenkata Ganesh Bolisetty <bsrivenk@codeaurora.org>
2018-02-16 15:59:32 +0530

    apollo: prop: Enable IO Cgroups
    Set the property "ro.vendor.iocgrp.config=1"
    Ensures performance of fore-ground application
    doesn't get effected by back-ground applications.
    
    Change-Id: I48a4cb019eb597dd49bd61eebef09fec5f27a775

--- product_prop.mk
+++ product_prop.mk
@@ -7,3 +7,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
    persist.camera.HAL3.enabled=1 \
    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord   
+
+# IO Cgroups
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.iocgrp.config=1
