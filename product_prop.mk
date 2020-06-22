7a7c2ab128367233258a2439dcdd587f71b742d3
Chenyang Zhong <zhongcy95@gmail.com>
2020-06-22 12:16:07 -0400

    apollo: prop: Enable Seamless Transfer
    It allows users to switch between audio output devices by tapping the music
    note icon on the notification of a music app.
    
    Change-Id: I72bd74bd7eb6d70dcc7e5613121b9ecdbfe0b0e8
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -1,13 +1,17 @@
-# IORap
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.iorapd.enable=true
- 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
    persist.camera.HAL3.enabled=1 \
    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord   
 
+# IORap
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.iorapd.enable=true
+
 # IO Cgroups
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.iocgrp.config=1
+
+# Seamless Transfer
+PRODUCT_PRODUCT_PROPERTIES += \
+    sys.fflag.override.settings_seamless_transfer=true
