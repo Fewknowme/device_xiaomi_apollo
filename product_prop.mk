0174aa521381b029b14af920ce68f7cd4a0e656d
TTTT555 <albukerko@gmail.com>
2020-11-23 02:16:14 +0600

    Apollo: Add camera aux package list
    - Required for third party apps to use their built in camera e.g Telegram
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I22ab565d7d75e278faf4600cd5aecec59164bf8f
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,9 @@
 # IORap
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.iorapd.enable=true
+ 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+   persist.camera.HAL3.enabled=1 \
+   vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
+   vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord   
